d2d6a9572df3026a0441baeaeb30dce1f628a54d
Pierrick Roger <pk.roger@icloud.com>
2018-05-21 20:01:41 +0200

    Print object in dbg msg.

--- lib/galaxy/jobs/__init__.py
+++ lib/galaxy/jobs/__init__.py
@@ -79,7 +79,7 @@ class JobDestination(Bunch):
         if self.tags is not None:
             self['tags'] = [x.strip() for x in self.tags.split(',')]
             
-        log.debug("JobDestination created with id %s." % self['id'])
+        log.debug("JobDestination: %r." % self)
 
 class JobToolConfiguration(Bunch):
     """
